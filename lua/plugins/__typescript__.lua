f529b8214960079967c3d9ba698105a3d275061a
Samyak Bardiya <mail@samyakbardiya.dev>
2024-12-18 02:53:55 +0530

    Wed Dec 18 02:53:55 AM IST 2024

--- lua/plugins/__typescript__.lua
+++ lua/plugins/__typescript__.lua
@@ -1,8 +1,29 @@
 return {
 
+    -- -- Neovim plugin to format tsserver LSP errors
+    -- {
+    --     "davidosomething/format-ts-errors.nvim",
+    -- },
+
+    -- -- for turning messy and confusing TypeScript errors into plain English.
+    -- {
+    --     "dmmulroy/ts-error-translator.nvim",
+    -- },
+
     -- better formatted TypeScript errors
     {
         "OlegGulevskyy/better-ts-errors.nvim",
         dependencies = { "MunifTanjim/nui.nvim" },
+        config = {
+            keymaps = {
+                toggle = "<leader>cp",
+                go_to_definitation = "<leader>c.",
+            },
+        },
+    },
+
+    --  Use treesitter to auto close and auto rename html tag
+    {
+        "nvim-ts-autotag",
     },
 }
---
+++ lua/plugins/monorepo.lua
@@ -0,0 +1,14 @@
+-- An effective way to juggle monorepos in neovim
+return {
+    "imNel/monorepo.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim",
+    },
+    config = true,
+    -- config = function()
+    --     require("monorepo").setup({
+    --         -- Your config here!
+    --     })
+    -- end,
+}

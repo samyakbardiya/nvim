2c23907aad87e9b9fd4df5609f7adaaad2e7eae2
Samyak Bardiya <mail@samyakbardiya.dev>
2024-12-26 17:21:40 +0530

    Thu Dec 26 17:21:40 IST 2024

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,23 +1,32 @@
--- Performant, batteries-included completion plugin for Neovim
 return {
-    "saghen/blink.cmp",
-    dependencies = {
-        "Gelio/cmp-natdat",
-        "kirasok/cmp-hledger",
+    {
+        -- Compatibility layer for using nvim-cmp sources on blink.cmp
+        "saghen/blink.compat",
+        lazy = true,
+        opts = {},
+        version = not vim.g.lazyvim_blink_main and "*",
     },
-    opts = {
-        sources = {
-            compat = { "hledger", "natdat" },
-            provider = {
-                hledger = { name = "hledger" },
-                natdat = { name = "natdat" },
-            },
+    {
+        -- Performant, batteries-included completion plugin for Neovim
+        "saghen/blink.cmp",
+        dependencies = {
+            "Gelio/cmp-natdat",
+            "kirasok/cmp-hledger",
         },
-        keymap = {
-            preset = "default",
-            ["<C-a>"] = { "select_and_accept" },
-            ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-            ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+        opts = {
+            sources = {
+                compat = { "hledger", "natdat" },
+                providers = {
+                    hledger = { name = "hledger" },
+                    natdat = { name = "natdat" },
+                },
+            },
+            keymap = {
+                preset = "default",
+                ["<C-a>"] = { "select_and_accept" },
+                ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+            },
         },
     },
 }
